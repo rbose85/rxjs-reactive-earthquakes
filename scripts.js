518ca0f8ff3e0d5e23fe6f66f277f49df721b716
Rajiv Bose <rbose85@users.noreply.github.com>
2016-04-11 07:26:29 +0100

    preferred starting point for tutorial

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 const map = L.map('map').setView([33.858631, -118.279602], 7);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-const quakes = Rx.DOM.jsonpRequest({
-  url: 'http://earthquakes.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp',
-  jsonpCallback: 'eqfeed_callback'
-})
+const quakes = Rx.Observable.interval(5000)
+    .flatMap(() => Rx.DOM.jsonpRequest({
+          url: 'http://earthquakes.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp',
+          jsonpCallback: 'eqfeed_callback'
+        })
+            .retry(3)
+    )
     .flatMap(result => Rx.Observable.from(result.response.features))
-    .map(quake => ({
-      lat: quake.geometry.coordinates[1],
-      lng: quake.geometry.coordinates[0],
-      size: quake.properties['mag'] * 10000
-    }));
+    .distinct(quake => quake.properties.code);
 
 quakes.subscribe(quake => {
-  L.circle([quake.lat, quake.lng], quake.size).addTo(map);
+  const coords = quake.geometry.coordinates;
+  const size = quake.properties['mag'] * 10000;
+
+  L.circle([coords[1], coords[0]], size).addTo(map);
 });
