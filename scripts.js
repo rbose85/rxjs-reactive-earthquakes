ad80a81871c4000fb861e899f6a31cebc9ff892f
Rajiv Bose <rbose85@users.noreply.github.com>
2016-04-11 07:14:38 +0100

    simplest working example using jsonpRequest() stream to render earthquakes

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 const map = L.map('map').setView([33.858631, -118.279602], 7);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+const quakes = Rx.DOM.jsonpRequest({
+  url: 'http://earthquakes.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp',
+  jsonpCallback: 'eqfeed_callback'
+})
+    .flatMap(result => Rx.Observable.from(result.response.features))
+    .map(quake => ({
+      lat: quake.geometry.coordinates[1],
+      lng: quake.geometry.coordinates[0],
+      size: quake.properties['mag'] * 10000
+    }));
+
+quakes.subscribe(quake => {
+  L.circle([quake.lat, quake.lng], quake.size).addTo(map);
+});
